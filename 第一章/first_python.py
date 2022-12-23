d243c72fd7023171a4dd444ea914a6d12bf6296e
zzcl365 <menghuanmigong@gmail.com>
2022-12-23 16:18:58 +0800

    乾颐堂python学习，第二次提交

--- 第一章/first_python.py
+++ 第一章/first_python.py
@@ -1,5 +1,11 @@
 print('Hello World')
 
+# 第一次提交
 print('qytang-Python-Learning 第1次变更')
 print('qytang-Python-Learning 第2次变更')
 print('qytang-Python-Learning 第3次变更')
+
+# 第二次提交
+print('qytang-Python-Learning 第4次变更')
+print('qytang-Python-Learning 第5次变更')
+print('qytang-Python-Learning 第6次变更')
