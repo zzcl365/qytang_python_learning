47743fdbf24d8a03d2291e85b3bca485358d2673
zzcl365 <menghuanmigong@gmail.com>
2022-12-23 16:17:49 +0800

    乾颐堂python学习，第一次提交

--- 第一章/first_python.py
+++ 第一章/first_python.py
@@ -1,19 +1,5 @@
 print('Hello World')
-print('Python-Learning 第1次变更')
-print('Python-Learning 第2次变更')
-print('Python-Learning 第3次变更')
-print('Python-Learning 第4次变更')
-print('Python-Learning 第5次变更')
-print('Python-Learning 第6次变更')
 
 print('qytang-Python-Learning 第1次变更')
 print('qytang-Python-Learning 第2次变更')
 print('qytang-Python-Learning 第3次变更')
-
-print('qytang-Python-Learning 第4次变更')
-print('qytang-Python-Learning 第5次变更')
-print('qytang-Python-Learning 第6次变更')
-
-print('qytang-Python-Learning 第7次变更')
-print('qytang-Python-Learning 第8次变更')
-print('qytang-Python-Learning 第9次变更')
