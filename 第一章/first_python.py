46ed58b05608ab46a5bda67bbedbf356fc1aefd2
zzcl365 <menghuanmigong@gmail.com>
2022-12-23 16:14:42 +0800

    乾颐堂python学习，第三次提交

--- 第一章/first_python.py
+++ 第一章/first_python.py
@@ -10,7 +10,10 @@ print('qytang-Python-Learning 第1次变更')
 print('qytang-Python-Learning 第2次变更')
 print('qytang-Python-Learning 第3次变更')
 
-
 print('qytang-Python-Learning 第4次变更')
 print('qytang-Python-Learning 第5次变更')
 print('qytang-Python-Learning 第6次变更')
+
+print('qytang-Python-Learning 第7次变更')
+print('qytang-Python-Learning 第8次变更')
+print('qytang-Python-Learning 第9次变更')
