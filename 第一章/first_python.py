4001f90dd5f2f4111b71ba2c7860c2618ee8cd86
zzcl365 <menghuanmigong@gmail.com>
2022-12-23 16:05:54 +0800

    pycharm 测试
    前三次，代码变更

--- 第一章/first_python.py
+++ 第一章/first_python.py
@@ -2,4 +2,7 @@ print('Hello World')
 print('Python-Learning 第1次，变更')
 print('Python-Learning 第2次，变更')
 print('Python-Learning 第3次，变更')
+print('Python-Learning 第4次，变更')
+print('Python-Learning 第5次，变更')
+print('Python-Learning 第6次，变更')
 
